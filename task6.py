9b9554e84a66f8da6f2dccbdff5b3465e3c882dd
влад <vladickpochta@mail.ru>
2023-07-07 17:32:43 +0900

    Мною был проделан 1-ый блок

---
+++ task6.py
@@ -0,0 +1,8 @@
+#Напишите программу, которая принимает строку и делает заглавной первую букву
+#каждого слова в этой строке.
+
+
+
+string = input("Введите строку: ")
+capitalized_string = string.title()
+print(capitalized_string)
