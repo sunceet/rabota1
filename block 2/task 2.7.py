17e7b2704ff89dfa72f0627769bb554f1dee56ad
sunceet <138203222+sunceet@users.noreply.github.com>
2023-07-20 14:09:44 +0900

    Add files via upload

---
+++ block 2/task 2.7.py
@@ -0,0 +1,11 @@
+def sum_positive_numbers():
+    sum = 0
+    while True:
+        number = int(input("Введите число: "))
+        if number < 0:
+            break
+        sum += number
+    return sum
+
+result = sum_positive_numbers()
+print("Сумма положительных чисел:", result)
