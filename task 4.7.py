7b0921e3892db35e1487284eb8bf6fcf2170336a
sunceet <138203222+sunceet@users.noreply.github.com>
2023-07-20 17:59:03 +0900

    Add files via upload

---
+++ task 4.7.py
@@ -0,0 +1,11 @@
+import itertools
+
+def find_permutations(lst):
+    permutations = list(itertools.permutations(lst))
+    return permutations
+
+lst = input("Введите элементы списка через пробел: ").split()
+permutations = find_permutations(lst)
+print("Все перестановки заданного списка:")
+for permutation in permutations:
+    print(permutation)
