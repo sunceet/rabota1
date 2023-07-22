869e94c18da28e5ead23294b76d4f0a1012a664a
влад <vladickpochta@mail.ru>
2023-07-22 22:13:31 +0900

    скрыл лямбду

--- task 3.2.py
+++ task 3.2.py
@@ -1,7 +1,7 @@
-lo = lambda x: [0, 1] \
-    if x <= 2 else lo(x - 1) + [lo(x - 1)[-1] + lo(x - 1)[-2]]
+fi = lambda n: [0, 1] \
+    if n <= 2 else fi(n - 1) + [fi(n - 1)[-1] + fi(n - 1)[-2]]
 
 n = 10
-result = lo(n)
+result = fi(n)
 
 print("Список чисел Фибоначчи до", n, ":", result)
