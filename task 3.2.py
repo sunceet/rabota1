8cc4cb37858049858091ed01e70b0dfed3dcb4c0
влад <vladickpochta@mail.ru>
2023-07-22 21:51:11 +0900

    Исправил третий раз ошибки

--- task 3.2.py
+++ task 3.2.py
@@ -1,6 +1,7 @@
-l = lambda x: [0, 1] if x <= 2 else l(x - 1) + [l(x - 1)[-1] + l(x - 1)[-2]]
+lo = lambda x: [0, 1] \
+    if x <= 2 else lo(x - 1) + [lo(x - 1)[-1] + lo(x - 1)[-2]]
 
 n = 10
-result = l(n)
+result = lo(n)
 
 print("Список чисел Фибоначчи до", n, ":", result)
