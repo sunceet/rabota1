4a1676477647fc6a0506fc89feb54023a474db97
sunceet <138203222+sunceet@users.noreply.github.com>
2023-07-22 14:27:41 +0900

    Update task6.py

--- block 1/task6.py
+++ block 1/task6.py
@@ -1,8 +1,3 @@
-#Напишите программу, которая принимает строку и делает заглавной первую букву
-#каждого слова в этой строке.
-
-
-
 string = input("Введите строку: ")
 capitalized_string = string.title()
 print(capitalized_string)
