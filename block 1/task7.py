391be076a53cc67f741ab36872083fb146abb13a
sunceet <138203222+sunceet@users.noreply.github.com>
2023-07-22 14:27:53 +0900

    Update task7.py

--- block 1/task7.py
+++ block 1/task7.py
@@ -1,6 +1,3 @@
-#Напишите программу, которая принимает строку и меняет регистр всех букв на
-#противоположный.
-
 string = input("Введите строку: ")
 inverted_string = string.swapcase()
 print(inverted_string)
